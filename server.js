47bebf8ca45063e106ea7cea9f9b90255df1155e
Geri MacBook Pro <macbook@Geri-MacBook-Pro.local>
2026-08-14 09:28:34 -0500

    Add node:crypto polyfill for CDP JWT Ed25519 generation

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import crypto from 'node:crypto';
+if (!globalThis.crypto) globalThis.crypto = crypto;
 import { generateJwt } from '@coinbase/cdp-sdk/auth';
 
 const app = express();
